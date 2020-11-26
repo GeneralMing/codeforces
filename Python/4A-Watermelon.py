a11408ae0c705077ce0c6f231fae081c36499eae
GeneralMing <advaithkrishna7@gmail.com>
2020-11-26 20:35:48 +0530

    Added python solution for 4A

---
+++ Python/4A-Watermelon.py
@@ -0,0 +1,13 @@
+"""
+	Solution to Codeforces problem 4A
+	Copyright (c) GeneralMing. All rights reserved.
+
+	https://github.com/GeneralMing/codeforces
+"""
+
+w = int(input())
+
+if(w%2 != 0 or w == 2):
+	print("NO")
+else:
+	print("YES")
